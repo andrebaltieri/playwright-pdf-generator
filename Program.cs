302f17218596242d4191fb3041d40cf5dc1ba0bd
Andre Baltieri <andrebaltieri@hotmail.com>
2021-08-15 12:08:54 -0300

    Finalizado demo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://balta.io/blog/linguagens-de-programacao");
+        // await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+        await page.PdfAsync(new PagePdfOptions { Path = "linguagens-de-programacao.pdf" });
+    }
+}
